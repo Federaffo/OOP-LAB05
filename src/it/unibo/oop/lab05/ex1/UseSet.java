ced57c61a1a33c50ea0618e186ea0634143d098e
Federico Raffoni <36224988+Federaffo@users.noreply.github.com>
2020-11-18 14:21:06 +0100

    First push on github

---
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -0,0 +1,58 @@
+package it.unibo.oop.lab05.ex1;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Example class using {@link Set}.
+ * 
+ */
+public final class UseSet {
+
+    private UseSet() {
+    }
+
+    /**
+     * @param args
+     *            ignored
+     */
+    public static void main(final String[] args) {
+        /*
+         * Considering the content of "UseCollection, write a program which, in
+         * order:
+         * 
+         * 1) Builds a TreeSet containing Strings
+         * 
+         * 2) Populates such Collection with all the Strings ranging from "1" to
+         * "20"
+         * 
+         * 3) Prints its content
+         * 
+         * 4) Removes all those strings whose represented number is divisible by
+         * three
+         * 
+         * 5) Prints the content of the Set using a for-each costruct
+         * 
+         * 6) Verifies if all the numbers left in the set are even
+         */
+    	Set<String> mySet = new TreeSet<>();
+    	
+    	for(int i = 1; i < 20; i++) {
+    		mySet.add(Integer.toString(i));
+    	}
+    	System.out.println(mySet);
+    	
+    	Iterator<String> iterator = mySet.iterator();
+    	
+    	while (iterator.hasNext()) {
+			if(Integer.parseInt(iterator.next()) % 3 == 0)
+				iterator.remove();
+		}
+    	
+    	for (String x : mySet) {
+			System.out.println(x);
+		}
+    	
+    }
+}
